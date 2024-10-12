e41bbcbb9e3ab3f5e71d4acc4b70e9dd5b3f4e40
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-11 19:37:44 -0500

    chore: initial commit

---
+++ src/test/java/pe/upc/catchupplatform/CatchUpPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.catchupplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchUpPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
